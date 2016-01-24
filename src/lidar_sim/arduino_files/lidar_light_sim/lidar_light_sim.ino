ad882320c27ada0f459693aa71e73d8274ff8b5d
abhi <abhi@abhi-MacBookAir.(none)>
2016-01-24 14:43:49 -0800

    fixed bugs

--- src/lidar_sim/arduino_files/lidar_light_sim/lidar_light_sim.ino
+++ src/lidar_sim/arduino_files/lidar_light_sim/lidar_light_sim.ino
@@ -1,4 +1,4 @@
-int ranges[]={10,20,30,40,50};
+int ranges[]={10,20,30,40,255};
 #define size_of_int  2 // 2 bytes
 String start_check;
 boolean sys_ok_flag = false;
@@ -14,20 +14,23 @@ void setup()
 void loop()
 {
  if(!sys_ok_flag){
-  while (!Serial.available());
   start_check = String(Serial.read(), HEX);
-  if(start_check == "9f")
+  if(start_check == "9f"){
    sys_ok_flag = true;
+  }
   else
    sys_ok_flag = false;
  }
- 
- if(sys_ok_flag) {
-  Serial.print(78, HEX);
-  for(int i=0;i<5;i++)
+ if(sys_ok_flag) { 
+  Serial.print(78,HEX); 
+   for(int i=0;i<5;i++){
+   Serial.print(',');
    Serial.print(ranges[i]);
+  }
+  Serial.print(',');
+  Serial.print(79,HEX);
+  Serial.print('\n');
  }
   delay(100); 
-  
 }
 
--- src/lidar_sim/src/lidar_sim.py
+++ src/lidar_sim/src/lidar_sim.py
@@ -4,25 +4,36 @@ from sensor_msgs.msg import LaserScan
 import serial
 import sys
 import time
-
-data_size = 5;
+import numpy
 
 
 def pub_scan():
- while(hex(int(check_data,16)) == hex(78)):
-  data_port.write("\x9f")
-  check_data = data_port.read(2)
+ data_port.write("\x9f")
+ #if(hex(int(check_data,16)) == hex(78)):
+ range_data = data_port.readline()
+ range_data = data_port.readline()
  while not rospy.is_shutdown():
-    for i in range(1,data_size+1):
-  	 data = data_port.read()
-  	 rospy.loginfo(data) 
- 
- 
+    range_data = data_port.readline()
+    range_data = range_data.rstrip(",4F\n")                                                                                                                          
+    range_data = range_data.lstrip("4E ,")
+    rospy.loginfo(range_data)
+    range_data = map(float, range_data.split(','))
+    scan_msg.header.stamp = rospy.get_rostime()
+    scan_msg.ranges = range_data;
+    scan_publisher.publish(scan_msg)
+    rospy.loginfo(range_data)
 
 if __name__ == '__main__':
  rospy.init_node("lidar_sim")
  rate = rospy.Rate(0.5)
  scan_publisher = rospy.Publisher("/lidar_scan",LaserScan, queue_size=1000)
+ scan_msg = LaserScan()
+ scan_msg.header.frame_id = "laser_frame"
+ scan_msg.angle_min = 0;
+ scan_msg.angle_max = 180;
+ scan_msg.angle_increment = 10;
+ scan_msg.range_min = 0;
+ scan_msg.range_max = 80;
  data_port = serial.Serial(
     port='/dev/ttyUSB0',
     baudrate=9600,
