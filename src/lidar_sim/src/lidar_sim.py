bebbe226792d45f3577fdf5a7cbb0aa7ad6747dd
Abhishek Goudar <deloney21@gmail.com>
2016-01-24 22:32:50 -0800

    modified

--- src/lidar_sim/src/lidar_sim.py
+++ src/lidar_sim/src/lidar_sim.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import LaserScan 
+from std_msgs.msg import Bool
 import serial
 import sys
 import time
 import numpy
 
 
-def pub_scan():
- data_port.write("\x9f")
- #if(hex(int(check_data,16)) == hex(78)):
- range_data = data_port.readline()
- range_data = data_port.readline()
- while not rospy.is_shutdown():
-    range_data = data_port.readline()
-    range_data = range_data.rstrip(",4F\n")                                                                                                                          
-    range_data = range_data.lstrip("4E ,")
-    rospy.loginfo(range_data)
-    range_data = map(float, range_data.split(','))
-    scan_msg.header.stamp = rospy.get_rostime()
-    scan_msg.ranges = range_data;
-    scan_publisher.publish(scan_msg)
-    rospy.loginfo(range_data)
+def pub_scan(generate_scan):
+ if generate_scan:
+  rospy.loginfo("Generating scan\n")
+  data_port.write("\x9f")
+  #if(hex(int(check_data,16)) == hex(78)):
+  range_data = data_port.readline()
+  range_data = data_port.readline()
+  range_data = range_data.rstrip(",4F\n")                                                                                                                          
+  range_data = range_data.lstrip("4E ,")
+  rospy.loginfo(range_data)
+  range_data = map(float, range_data.split(','))
+  scan_msg.header.stamp = rospy.get_rostime()
+  scan_msg.ranges = range_data;
+  scan_publisher.publish(scan_msg)
+  rospy.loginfo(range_data)
 
 if __name__ == '__main__':
  rospy.init_node("lidar_sim")
  rate = rospy.Rate(0.5)
  scan_publisher = rospy.Publisher("/lidar_scan",LaserScan, queue_size=1000)
+ scan_trigger = rospy.Subscriber("/generate_scan",Bool,pub_scan)
  scan_msg = LaserScan()
  scan_msg.header.frame_id = "laser_frame"
  scan_msg.angle_min = 0;
@@ -41,8 +43,12 @@ if __name__ == '__main__':
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS
  )
- pub_scan()
-
+ data_port.write("\x9f")
+ data_port.write("\x9f")
+ time.sleep(2)
+ range_data = data_port.readline()
+ range_data = data_port.readline()
+ rospy.spin()
 
 
   
