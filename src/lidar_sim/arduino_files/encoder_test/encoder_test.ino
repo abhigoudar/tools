8fd945604a4cef64ac594f4e840c894775497bd1
Abhishek Goudar <deloney21@gmail.com>
2016-01-31 20:04:40 -0800

    working commit

--- src/lidar_sim/arduino_files/encoder_test/encoder_test.ino
+++ src/lidar_sim/arduino_files/encoder_test/encoder_test.ino
@@ -1,17 +1,90 @@
-int led = 13;
-int encoder_input_pin = 12;
-byte encoder_data;
-void setup() {                
-  pinMode(led, OUTPUT);
-  pinMode(encoder_input_pin, INPUT);
+#include <Servo.h>
+#include <Wire.h>
+
+#define LIDARLite_ADDRESS 0x62 // Default I2C Address of LIDAR-Lite.
+#define RegisterMeasure 0x00 // Register to write to initiate ranging.
+#define MeasureValue 0x04 // Value to initiate ranging.
+#define RegisterHighLowB 0x8f // Register to get both High and Low bytes in 1 call.
+
+
+Servo myservo;
+
+int scan_data;
+byte servo_pin=3;
+int pos=0;
+String start_check;
+volatile double encoder_data = 0;
+bool sys_ok_flag = false;
+int reading;
+
+void setup() {    
+  Wire.begin(); 
+  Serial.begin(9600);
+  while(!Serial);
+  myservo.attach(servo_pin);
+  attachInterrupt(0, increment_encoder, CHANGE);
+}
+void loop() {
+  if(!sys_ok_flag){
+  start_check = String(Serial.read(), HEX);
+  if(start_check == "9f"){
+   sys_ok_flag = true;
+  }
+  else
+   sys_ok_flag = false;
+ }
+  if(sys_ok_flag) { 
+  pos = 0;
+  myservo.write(pos);
+  delay(10);
+  Serial.print(78, HEX);
+  for(pos = 1; pos <180; pos += 1)   
+  {                                   
+    myservo.write(pos); 
+    Serial.print(',');
+    Serial.print(float(get_range_data()*0.01));
+  } 
+  Serial.print(',');
+  Serial.print(79,HEX);
+  Serial.print('\n');
+  delay(100);
+  pos = 180;
+  Serial.print(78, HEX);
+  for(pos = 180; pos>=1; pos-=1)      
+  {                                
+    myservo.write(pos);               
+    Serial.print(',');
+    Serial.print(float(get_range_data()*0.01));
+  } 
+  Serial.print(',');
+  Serial.print(79,HEX);  
+  Serial.print('\n');
+ }
+ sys_ok_flag = false;
+}
+void increment_encoder(){
+ encoder_data = (encoder_data + 1);
 }
 
-void loop() {
-  encoder_data = digitalRead(encoder_input_pin);
-  
-  if(encoder_data)
-   digitalWrite(led,HIGH);
-  else
-   digitalWrite(led, LOW);
-  
+int get_range_data()
+{
+ Wire.beginTransmission((int)LIDARLite_ADDRESS); 
+ Wire.write((int)RegisterMeasure); 
+ Wire.write((int)MeasureValue); 
+ Wire.endTransmission();
+ delay(20); 
+
+ Wire.beginTransmission((int)LIDARLite_ADDRESS); 
+ Wire.write((int)RegisterHighLowB); 
+ Wire.endTransmission(); 
+ delay(20); 
+ 
+ Wire.requestFrom((int)LIDARLite_ADDRESS, 2); 
+ if(2 <= Wire.available()) 
+   {
+     reading = Wire.read(); 
+     reading = reading << 8; 
+     reading |= Wire.read(); 
+   }
+  return reading;
 }
--- src/lidar_sim/src/lidar_sim.py
+++ src/lidar_sim/src/lidar_sim.py
@@ -14,12 +14,19 @@ def pub_scan(generate_scan):
   data_port.write("\x9f")
   #if(hex(int(check_data,16)) == hex(78)):
   range_data = data_port.readline()
+  range_data = range_data.rstrip(",4F\n")                                                                                                                          
+  range_data = range_data.lstrip("4E ,")
+  range_data = map(float, range_data.split(','))
+  scan_msg.header.stamp = rospy.get_rostime()
+  scan_msg.ranges = range_data;
+  scan_publisher.publish(scan_msg)
+  rospy.loginfo(range_data)
   range_data = data_port.readline()
   range_data = range_data.rstrip(",4F\n")                                                                                                                          
   range_data = range_data.lstrip("4E ,")
-  rospy.loginfo(range_data)
   range_data = map(float, range_data.split(','))
   scan_msg.header.stamp = rospy.get_rostime()
+  range_data = range_data[::-1]
   scan_msg.ranges = range_data;
   scan_publisher.publish(scan_msg)
   rospy.loginfo(range_data)
@@ -32,22 +39,18 @@ if __name__ == '__main__':
  scan_msg = LaserScan()
  scan_msg.header.frame_id = "laser_frame"
  scan_msg.angle_min = 0;
- scan_msg.angle_max = 180;
- scan_msg.angle_increment = 10;
+ scan_msg.angle_max = 3.14;
+ scan_msg.angle_increment = 0.01744;
  scan_msg.range_min = 0;
  scan_msg.range_max = 80;
  data_port = serial.Serial(
-    port='/dev/ttyUSB0',
+    port='/dev/ttyACM0',
     baudrate=9600,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS
  )
- data_port.write("\x9f")
- data_port.write("\x9f")
  time.sleep(2)
- range_data = data_port.readline()
- range_data = data_port.readline()
  rospy.spin()
 
 
